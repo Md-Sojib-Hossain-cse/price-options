3ad23dcfc33a808b5fb070f0c0dbf4bfcadbd810
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-03-19 20:15:13 +0600

    list items added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
+// import DaisyNav from './Components/DaisyNav/DaisyNav'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-7xl'>Vite + React</h1>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
     </>
   )
 }
