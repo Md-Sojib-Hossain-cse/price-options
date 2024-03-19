ec1eafda1c1ed3fadb822d0a71ad94bdb54c15f7
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-03-19 20:38:22 +0600

    toggling icon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
+import { MdMenuOpen } from "react-icons/md";
+import { IoIosClose } from "react-icons/io";
+
+
 // import DaisyNav from './Components/DaisyNav/DaisyNav'
 import Navbar from './Components/Navbar/Navbar'
+import { useState } from 'react';
 
 function App() {
+  const [open, setOpen] = useState(false);
 
   return (
     <>
       {/* <DaisyNav></DaisyNav> */}
+      <div onClick={() => setOpen(!open)} className='text-xl md:hidden'>
+        {open === true ? <IoIosClose /> : <MdMenuOpen />}
+      </div>
       <Navbar></Navbar>
     </>
   )
